582735c39aa26e4131deb70f26f299e7809a1dfe
Judit <juditrubio27@gmail.com>
2025-02-06 10:55:11 +0100

    error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import {
   faPython,
   faReact,
